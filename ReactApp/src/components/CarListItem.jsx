de9cb8a6bb879dfcd57fefa96605a745f34fc262
Christopher Krügelstein <christopher.kruegelstein@nexenio.com>
2017-06-08 17:52:03 +0200

    new folder in our repo

---
+++ ReactApp/src/components/CarListItem.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import '../../style/CarListItem.css';
+
+class CarListItem extends Component {
+  render() {
+    return (
+      <div className="carItem">
+        <h4 className="carItemText">Car-XYZ</h4>
+      </div>
+    )
+  }
+}
+
+export default CarListItem;
---
+++ ReactApp/src/components/SelectedCarList.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Car from './Car';
+// import '../../style/SelectedCarList.css';
+
+class SelectedCarList extends Component {
+  render() {
+    return (
+      <div className="selCarList col-md-3">
+        <h4 className="selCarHeader">Selected Cars</h4>
+        <div className="selCarContainer">
+          <Car />
+          <Car />
+          <Car />
+          <Car />
+          <Car />
+          <Car />
+          <Car />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SelectedCarList;
