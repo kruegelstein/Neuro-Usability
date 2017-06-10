fee8eb9abc6eff87c6a5c7bea4c2ca6b0401e75f
Christopher Krügelstein <christopher.kruegelstein@nexenio.com>
2017-06-11 01:41:11 +0200

    checks in all cars when cars are selected

--- ReactApp/src/components/CarListItem.jsx
+++ ReactApp/src/components/CarListItem.jsx
@@ -9,13 +9,22 @@ const CarListItem = ({ name, onClick, onEdit, onUnselect, filterSelected, isSele
         {name}
       </a>
       <div className={`iconContainer ${(filterSelected || isSelected) ? '' : 'hidden'}`}>
-        <a className="x" onClick={onUnselect}>X</a>
-          <a
-            className="edit"
-            onClick={onEdit}
-            >
-            <i className="fa fa-car" aria-hidden="true"></i>
-          </a>
+        <a className={`x ${filterSelected ? '' : 'hidden'}`} onClick={onUnselect}>X</a>
+          { (!filterSelected && isSelected) ?
+            <a
+              className="edit"
+              onClick={onEdit}
+              >
+              <i className='fa fa-check-circle-o' aria-hidden="true"></i>
+            </a>
+           :
+            <a
+              className="edit"
+              onClick={onEdit}
+              >
+              <i className='fa fa-car' aria-hidden="true"></i>
+            </a>
+          }
       </div>
     </li>
   )
